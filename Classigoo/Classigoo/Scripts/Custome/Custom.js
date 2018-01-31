93dcb0b3bc89b5c598fd388248e5bc664b926d32
Madhu <madhubabu559@gmail.com>
2018-01-31 23:51:46 +0530

    Added Fb Login

--- Classigoo/Classigoo/Scripts/Custome/Custom.js
+++ Classigoo/Classigoo/Scripts/Custome/Custom.js
@@ -1,22 +1,35 @@
-﻿function RegisterUser(profile) {
+﻿function RegisterUser(profile,type) {
     jQuery.support.cors = true;
-    var user = {
+    var user = {};
+    if (type = "Gmail")
+    {
+      
+        user.Name=profile.getName();
+        user.Type= type;
+        user.Email = profile.getEmail();
+       
+    }
+    else if (type = "Fb")
+    {
         
-        Name: profile.getName(),
-        Type:'Gmail',
-        Email: profile.getEmail()
-    };
-
+           user.Name=profile.name;
+           user.Type = type;
+           user.FbId = profile.id;
+       
+    }   
     $.ajax({
         url: 'http://localhost:51797/api/UserApi',
         type: 'POST',
         data: JSON.stringify(user),
         contentType: "application/json;charset=utf-8",
         success: function (data) {
-            WriteResponse(data);
+
+           
+            window.location.href = '<%= Url.Action("User", "Home") %>';
+           
         },
         error: function (x, y, z) {
-            alert(x + '\n' + y + '\n' + z);
+            console.log(x + '\n' + y + '\n' + z);
         }
     });
 }
