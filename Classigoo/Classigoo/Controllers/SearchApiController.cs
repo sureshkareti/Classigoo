d59863a99b7576b484e369bfd5b41ae76d920a38
madhubabu <madhubabu559@gmail.com>
2018-02-11 22:54:31 +0530

    Added Search controller

---
+++ Classigoo/Classigoo/Controllers/SearchApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Classigoo.Controllers
+{
+    public class SearchApiController : ApiController
+    {
+        private ClassigooEntities db = new ClassigooEntities();
+
+
+        [HttpGet]
+        [ActionName("GetAllAdds")]
+        public IHttpActionResult GetAllAdds()
+        {
+            var adds = db.Adds.ToList();
+            if (adds.Count > 0)
+                return Ok(adds);
+            else
+                return NotFound();
+
+
+        }
+    }
+}
---
+++ Classigoo/Classigoo/Controllers/SearchController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Classigoo.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult GetAllAdds()
+        {
+            List<Add> addColl = new List<Add>();
+            using (var client = new HttpClient())
+            {
+                string url = "http://localhost:51797/api/SearchApi/GetAllAdds/";
+                client.BaseAddress = new Uri(url);
+                //HTTP GET
+                var responseTask = client.GetAsync(url);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<List<Add>>();
+                    readTask.Wait();
+
+                    addColl = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+
+
+
+
+
+            return View("DisplayAdds", addColl);
+        }
+    }
+}
