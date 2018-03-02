bb8ced4252ef5473be7f0780be4db9548ae7fdb7
madhubabu <madhubabu559@gmail.com>
2018-03-02 15:38:52 +0530

    Added new layout

---
+++ Classigoo/Classigoo/Controllers/NewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Classigoo.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
