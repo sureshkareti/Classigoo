f3414d672a6b30d56d3bf66bad02021da791b3e1
madhubabu <madhubabu559@gmail.com>
2018-03-07 01:39:16 +0530

    Datatable for search

--- Classigoo/Classigoo/Controllers/NewController.cs
+++ Classigoo/Classigoo/Controllers/NewController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace Classigoo.Controllers
 {
     public class NewController : Controller
@@ -21,32 +20,132 @@ namespace Classigoo.Controllers
         {
             return View(GetAdds(1));
         }
-        [HttpPost]
-        public ActionResult DisplayAdds(int currentPageIndex)
-        {
-            return PartialView(GetAdds(currentPageIndex));
-        }
+        //[HttpPost]
+        //public ActionResult DisplayAdds(int currentPageIndex)
+        //{
+        //    return PartialView(GetAdds(currentPageIndex));
+        //}
 
         private AddsModel GetAdds(int currentPage)
         {
             int maxRows = 10;
             ClassigooEntities db = new ClassigooEntities();
             
-                AddsModel addColl = new AddsModel();
-
-                addColl.Adds = (from add in db.Adds
-                                           select add)
-                            .OrderBy(add=>add.Category)
-                            .Skip((currentPage - 1) * maxRows)
-                            .Take(maxRows).ToList();
-
-                double pageCount = (double)((decimal)db.Adds.Count() / Convert.ToDecimal(maxRows));
+            AddsModel addColl = new AddsModel();
+            List<CustomAdd> coll = new List<CustomAdd>();
+            List<Add> addsByPage = (from add in db.Adds
+                            select add)
+                        .OrderBy(add => add.Category)
+                        .Skip((currentPage - 1) * maxRows)
+                        .Take(maxRows).ToList();
+            foreach(var add in addsByPage)
+            {
+                coll.Add(CheckCategory(add));
+            }
+            addColl.Adds = coll;
+            double pageCount = (double)((decimal)db.Adds.Count() / Convert.ToDecimal(maxRows));
                 addColl.PageCount = (int)Math.Ceiling(pageCount);
 
                 addColl.CurrentPageIndex = currentPage;
 
                 return addColl;
             
+        }
+        public CustomAdd CheckCategory(Add add)
+        {
+            CustomAdd customAdd = new CustomAdd();
+            customAdd.Location = add.Location;
+            customAdd.Createddate = (DateTime)add.Created;
+            customAdd.AddId = add.AddId;
+            switch (add.Category)
+            {
+                case "RealEstate":
+                    { 
+                         foreach(var item in add.RealEstates)
+                        {
+                            customAdd.Description = item.Description;
+                            customAdd.Title = item.Title;
+                        }
+                        break;
+                    }
+                case "Cars":
+                    {
+                        foreach (var item in add.Cars)
+                        {
+                            customAdd.Description = item.Description;
+                            customAdd.Title = item.Title;
+                        }
+                        break;
+                    }
+                case "Electronics":
+                    {
+                        foreach (var item in add.Electronics)
+                        {
+                            customAdd.Description = item.Description;
+                            customAdd.Title = item.Title;
+                        }
+                        break;
+                    }
+                    
+            }
+            return customAdd;
+        }
+
+
+        public ActionResult GetDataForDatatable(JQueryDataTableParams param)
+        {
+            ClassigooEntities db = new ClassigooEntities();
+            IQueryable<Add> memberCol = db.Adds.AsQueryable();
+            int totalCount = memberCol.Count();
+            IEnumerable<Add> filteredMembers = memberCol;
+       
+            //if (!string.IsNullOrEmpty(param.sSearch))
+            //{
+            //    filteredMembers = memberCol
+            //            .Where(m => m.FirstName.Contains(param.sSearch) ||
+            //               m.LastName.Contains(param.sSearch) ||
+            //               m.Company.Contains(param.sSearch) ||
+            //               m.JobTitle.Contains(param.sSearch));
+            //}
+
+            //Func<Member, string> orderingFunction = (m => param.iSortCol_0 == 0 ? m.FirstName :
+            //                      sortIdx == 1 ? m.LastName :
+            //                      sortIdx == 2 ? m.Company :
+            //                      m.JobTitle);
+
+            //if (param.sSortDir_0 == "asc")
+            //    filteredMembers = filteredMembers.OrderBy(orderingFunction);
+            //else
+            //    filteredMembers = filteredMembers.OrderByDescending(orderingFunction);
+
+            var displayedMembers = filteredMembers
+                     .Skip(param.iDisplayStart)
+                     .Take(param.iDisplayLength);
+
+            var result = from a in displayedMembers
+                         select new CustomAdd {AddId=a.AddId,Location=a.Location,Createddate=(DateTime)a.Created,
+                         Title="dfadf",Description="rerwe"
+                         };
+
+            //AddsModel addColl = new AddsModel();
+            //List<CustomAdd> coll = new List<CustomAdd>();
+            //List<Add> addsByPage = (from add in db.Adds
+            //                        select add)
+            //            .OrderBy(add => add.Category)
+            //            .Skip(param.iDisplayStart)
+            //            .Take(param.iDisplayLength).ToList();
+            //foreach (var add in addsByPage)
+            //{
+            //    coll.Add(CheckCategory(add));
+            //}
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = totalCount,
+                iTotalDisplayRecords = filteredMembers.Count(),
+                aaData = result
+            },
+               JsonRequestBehavior.AllowGet);
         }
     }
 }
--- Classigoo/Classigoo/Models/CustomModels.cs
+++ Classigoo/Classigoo/Models/CustomModels.cs
@@ -5,81 +5,46 @@ using System.Web;
 
 namespace Classigoo
 {
-  
-
-    public class ParentCategory
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Price { get; set; }
-        public string Model { get; set; }
-        public string Year { get; set; }
-        public string Fuel { get; set; }
-        public string KMDriven { get; set; }
-        public string Description { get; set; }
-        public string ImgUrlPrimary { get; set; }
-        public string ImgUrlSeconday { get; set; }
-        public string ImgUrlThird { get; set; }
-        public string ImgUrlFourth { get; set; }
-        public string Type { get; set; }
-        public string SubCategory { get; set; }
-        public string LocationId { get; set; }
-        public Nullable<System.DateTime> Created { get; set; }
-        public Nullable<int> AddId { get; set; }
-
-       // public int Id { get; set; }
-        //public string Title { get; set; }
-        //public string Price { get; set; }
-        //public string Description { get; set; }
-        //public string ImgUrlPrimary { get; set; }
-        //public string ImgUrlSeconday { get; set; }
-        //public string ImgUrlThird { get; set; }
-        //public string ImgUrlFourth { get; set; }
-        //public string Type { get; set; }
-        //public string SubCategory { get; set; }
-        //public string LocationId { get; set; }
-        //public Nullable<System.DateTime> Created { get; set; }
-        //public Nullable<int> AddId { get; set; }
-
-       // public int Id { get; set; }
-        //public string Title { get; set; }
-        //public string Price { get; set; }
-        public string Availability { get; set; }
-        public string ListedBy { get; set; }
-        public string Furnishing { get; set; }
-        public string Bedrooms { get; set; }
-        public string SquareFeets { get; set; }
-        public string Squareyards { get; set; }
-      //  public string Description { get; set; }
-        public string TypeId { get; set; }
-        public string SubCategoryId { get; set; }
-        //public string LocationId { get; set; }
-        //public Nullable<System.DateTime> Created { get; set; }
-        //public Nullable<int> AddId { get; set; }
-        //public string ImgUrlPrimary { get; set; }
-        //public string ImgUrlSeconday { get; set; }
-        //public string ImgUrlThird { get; set; }
-        //public string ImgUrlFourth { get; set; }
-
-
-
-    }
-
     public class AddsModel
     {
-        ///<summary>
-        /// Gets or sets Adds.
-        ///</summary>
-        public List<Add> Adds { get; set; }
+        public List<CustomAdd> Adds { get; set; }
 
-        ///<summary>
-        /// Gets or sets CurrentPageIndex.
-        ///</summary>
         public int CurrentPageIndex { get; set; }
 
-        ///<summary>
-        /// Gets or sets PageCount.
-        ///</summary>
         public int PageCount { get; set; }
+    }
+    public class CustomAdd
+    {
+        public int AddId { get; set; }
+        public string Title { get; set; }
+        public DateTime Createddate { set; get; }
+        public string Description { set; get; }
+        public string Location { set; get; }
+    }
+    public class JQueryDataTableParams
+    {     
+        public string sEcho { get; set; }
+
+        public string sSearch { get; set; }
+
+        
+        public int iDisplayLength { get; set; }
+
+        public int iDisplayStart { get; set; }
+
+        
+        public int iColumns { get; set; }
+
+        
+        public int iSortingCols { get; set; }
+
+        
+        public string sColumns { get; set; }
+
+        
+        public int iSortCol_0 { get; set; }
+
+        
+        public string sSortDir_0 { get; set; }
     }
 }
