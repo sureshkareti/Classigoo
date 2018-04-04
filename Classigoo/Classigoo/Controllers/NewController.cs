b6216c44a60225cf5b93d4d1259d79b61e346660
Srinu <Srinu@DESKTOP-HNDSM1C>
2018-04-04 15:44:03 +0500

    Added electronic filter and code

--- Classigoo/Classigoo/Controllers/NewController.cs
+++ Classigoo/Classigoo/Controllers/NewController.cs
@@ -106,10 +106,10 @@ namespace Classigoo.Controllers
             switch (category)
             {
                 case "Cars":
-                    addColl = FilterCars(filterColl);
+                    addColl = FilterCar(filterColl);
                     break;
                 case "Electronics":
-                    addColl = FilterCars(filterColl);
+                    addColl = FilterElectronic(filterColl);
                     break;
                 case "RealEstate":
                     addColl = FilterRealEstate(filterColl);
@@ -118,7 +118,119 @@ namespace Classigoo.Controllers
 
             return PartialView("DisplayAdds", addColl);
         }
-        public AddsModel FilterCars(Dictionary<string, object> filterOptions)
+        public ActionResult ApplyFilters(string category)
+        {
+            //Dictionary<string, object> filterColl = new Dictionary<string, object>();
+            //JavaScriptSerializer j = new JavaScriptSerializer();
+            //object filters = j.Deserialize(filterOptions, typeof(object));
+            //filterColl = (Dictionary<string, object>)filters;
+            AddsModel addColl = new AddsModel();
+            switch (category)
+            {
+                case "Cars":
+                    addColl = FilterCars();
+                    break;
+                case "Electronics":
+                    addColl = FilterElectronics();
+                    break;
+                case "RealEstate":
+                    addColl = FilterRealEstates();
+                    break;
+            }
+
+            return PartialView("DisplayAdds", addColl);
+        }
+        public AddsModel FilterCars()
+        {
+            int currentPage = 1;
+            int maxRows = 5;
+            ClassigooEntities db = new ClassigooEntities();
+            AddsModel addColl = new AddsModel();
+           
+
+            int totalRowCount = db.Cars.Count();
+
+            List<CustomAdd> carColl = (from car in db.Cars
+                                       join add in db.Adds on car.AddId equals add.AddId
+                              
+                                       orderby car.AddId
+                                       select new CustomAdd
+                                       {
+                                           AddId = add.AddId,
+                                           Location = add.Location,
+                                           CreatedDate = add.Created.ToString(),
+                                           Title = car.Title,
+                                           Description = car.Description
+                                       }).Skip((currentPage - 1) * maxRows)
+                        .Take(maxRows).ToList();
+            addColl.Adds = carColl;
+            double pageCount = (double)((decimal)totalRowCount / Convert.ToDecimal(maxRows));
+            addColl.PageCount = (int)Math.Ceiling(pageCount);
+
+            addColl.CurrentPageIndex = currentPage;
+            return addColl;
+        }
+        public AddsModel FilterElectronics()
+        {
+            int currentPage = 1;
+            int maxRows = 5;
+            ClassigooEntities db = new ClassigooEntities();
+            AddsModel addColl = new AddsModel();
+
+
+            int totalRowCount = db.Electronics.Count();
+
+            List<CustomAdd> carColl = (from Electronic in db.Electronics
+                                       join add in db.Adds on Electronic.AddId equals add.AddId
+
+                                       orderby Electronic.AddId
+                                       select new CustomAdd
+                                       {
+                                           AddId = add.AddId,
+                                           Location = add.Location,
+                                           CreatedDate = add.Created.ToString(),
+                                           Title = Electronic.Title,
+                                           Description = Electronic.Description
+                                       }).Skip((currentPage - 1) * maxRows)
+                        .Take(maxRows).ToList();
+            addColl.Adds = carColl;
+            double pageCount = (double)((decimal)totalRowCount / Convert.ToDecimal(maxRows));
+            addColl.PageCount = (int)Math.Ceiling(pageCount);
+
+            addColl.CurrentPageIndex = currentPage;
+            return addColl;
+        }
+        public AddsModel FilterRealEstates()
+        {
+            int currentPage = 1;
+            int maxRows = 5;
+            ClassigooEntities db = new ClassigooEntities();
+            AddsModel addColl = new AddsModel();
+
+
+            int totalRowCount = db.RealEstates.Count();
+
+            List<CustomAdd> carColl = (from RealEstate in db.RealEstates
+                                       join add in db.Adds on RealEstate.AddId equals add.AddId
+
+                                       orderby RealEstate.AddId
+                                       select new CustomAdd
+                                       {
+                                           AddId = add.AddId,
+                                           Location = add.Location,
+                                           CreatedDate = add.Created.ToString(),
+                                           Title = RealEstate.Title,
+                                           Description = RealEstate.Description
+                                       }).Skip((currentPage - 1) * maxRows)
+                        .Take(maxRows).ToList();
+            addColl.Adds = carColl;
+            double pageCount = (double)((decimal)totalRowCount / Convert.ToDecimal(maxRows));
+            addColl.PageCount = (int)Math.Ceiling(pageCount);
+
+            addColl.CurrentPageIndex = currentPage;
+            return addColl;
+        }
+        public AddsModel FilterCar(Dictionary<string, object> filterOptions)
         {
             int currentPage = 1;
             int maxRows = 5;
@@ -231,8 +343,44 @@ namespace Classigoo.Controllers
             return addColl;
 
         }
-        public void FilterElectronics(Dictionary<string, object> filterOptions)
+        public AddsModel FilterElectronic(Dictionary<string, object> filterOptions)
         {
+
+            int currentPage = 1;
+            int maxRows = 5;
+            ClassigooEntities db = new ClassigooEntities();
+            AddsModel addColl = new AddsModel();
+
+            string pricefrom = filterOptions["pricefrom"].ToString();
+          
+
+            int totalRowCount = db.Electronics.Where(Electronic =>
+
+           
+           (pricefrom != "Price From" ? Electronic.Price == pricefrom : true)
+           
+            ).Count();
+
+            List<CustomAdd> electronicColl = (from Electronic in db.Electronics
+                                              join add in db.Adds on Electronic.AddId equals add.AddId
+                                              where
+                    (pricefrom != "Price From" ? Electronic.Price == pricefrom : true) 
+                                              orderby Electronic.AddId
+                                              select new CustomAdd
+                                              {
+                                                  AddId = add.AddId,
+                                                  Location = add.Location,
+                                                  CreatedDate = add.Created.ToString(),
+                                                  Title = Electronic.Title,
+                                                  Description = Electronic.Description
+                                              }).Skip((currentPage - 1) * maxRows)
+                        .Take(maxRows).ToList();
+            addColl.Adds = electronicColl;
+            double pageCount = (double)((decimal)totalRowCount / Convert.ToDecimal(maxRows));
+            addColl.PageCount = (int)Math.Ceiling(pageCount);
+
+            addColl.CurrentPageIndex = currentPage;
+            return addColl;
 
         }
     }
