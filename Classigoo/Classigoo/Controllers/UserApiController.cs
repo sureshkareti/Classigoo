b7b56fa8d579f5a0eb0357a16588fc0b7a6ab95c
madhubabu <madhubabu559@gmail.com>
2018-06-10 09:55:47 +0530

    logging

--- Classigoo/Classigoo/Controllers/UserApiController.cs
+++ Classigoo/Classigoo/Controllers/UserApiController.cs
@@ -11,6 +11,7 @@ using System.Web.Http.Description;
 using System.Web;
 using Classigoo;
 using System.Globalization;
+using Classigoo.Models;
 
 namespace Classigoo.Controllers
 {
@@ -36,48 +37,12 @@ namespace Classigoo.Controllers
 
             return Ok(user);
         }
-
-        // PUT: api/UsersApi/5
-        //[ResponseType(typeof(void))]
-        //public IHttpActionResult PutUser(Guid id, User user)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    if (id != user.UserId)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    db.Entry(user).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        db.SaveChanges();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!UserExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return StatusCode(HttpStatusCode.NoContent);
-        //}
-
-        // POST: api/UsersApi
         
         [HttpPost]
         [ActionName("AddUser")]
         public IHttpActionResult AddUser(User user)
         {
+           
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
@@ -94,23 +59,22 @@ namespace Classigoo.Controllers
             {
                 db.SaveChanges();
             }
-            catch (DbUpdateException)
+            catch (DbUpdateException ex)
             {
                 if (UserExists(user.UserId))
                 {
-                    return Conflict();
+                    Library.WriteLog("At adduser user already exist", ex);
+                   // return Conflict();
                 }
                 else
                 {
-                    throw;
+                    Library.WriteLog("At adduser", ex);
+                    // throw;
                 }
             }
 
             return Ok(user);
         }
-
-        // POST: api/UsersApi
-
         [HttpPost]
         [ActionName("AddLog")]
         public IHttpActionResult AddLog(Log log)
@@ -131,8 +95,6 @@ namespace Classigoo.Controllers
 
             return Ok(log);
         }
-
-        // DELETE: api/UsersApi/5
 
         [ResponseType(typeof(User))]
         public IHttpActionResult DeleteUser(Guid id)
@@ -166,17 +128,24 @@ namespace Classigoo.Controllers
         public IHttpActionResult CheckUser(string id,string type)
         {
             User user = new User();
-            if(type=="Gmail")
+            try
             {
-                user = db.Users.Where(e => e.Email == id).FirstOrDefault();
+                if (type == "Gmail")
+                {
+                    user = db.Users.Where(e => e.Email == id).FirstOrDefault();
+                }
+                else if (type == "Fb")
+                {
+                    user = db.Users.Where(e => e.FbId == id).FirstOrDefault();
+                }
+                else if (type == "Custom")
+                {
+                    user = db.Users.Where(e => e.MobileNumber == id).FirstOrDefault();
+                }
             }
-            else if(type=="Fb")
+            catch(Exception ex)
             {
-                user = db.Users.Where(e => e.FbId == id).FirstOrDefault();
-            }
-            else if(type=="Custom")
-            {
-                user = db.Users.Where(e => e.MobileNumber == id).FirstOrDefault();
+                Library.WriteLog("At checking user username- "+ id, ex);
             }
             if(user!=null)
             {
@@ -191,20 +160,26 @@ namespace Classigoo.Controllers
         [HttpGet]
         public IHttpActionResult IsValidUser(string userName, string pwd,string logintype)
         {
-            List<User> user = new List<User>();
-            if (logintype=="email")
+            User user = new User();
+            try
             {
-                 user = db.Users.Where(u => u.Email == userName).Where(u => u.Password == pwd).ToList();
+                if (logintype == "email")
+                {
+                    user = db.Users.Where(u => u.Email == userName).Where(u => u.Password == pwd).FirstOrDefault();
+                }
+                else if (logintype == "phone")
+                {
+                    user = db.Users.Where(u => u.MobileNumber == userName).Where(u => u.Password == pwd).FirstOrDefault();
+                }
             }
-            else if(logintype=="phone")
+            catch(Exception ex)
             {
-                user = db.Users.Where(u => u.MobileNumber == userName).Where(u => u.Password == pwd).ToList();
+                Library.WriteLog("At isvalid user username- "+userName, ex);
             }
             
-            if(user.Count()>0)
+            if(user!=null)
             {
-                
-                return Ok(user[0].UserId);
+                return Ok(user.UserId);
             }
             else
             {
@@ -218,32 +193,24 @@ namespace Classigoo.Controllers
         {
             try
             {
-              
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
+                //if (!ModelState.IsValid)
+                //{
+                //    return BadRequest(ModelState);
+                //}
                 db.Entry(user).State = EntityState.Modified;
 
                 try
                 {
                     db.SaveChanges();
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateConcurrencyException ex)
                 {
-                    if (!UserExists(user.UserId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    Library.WriteLog("At updating user details- dbupdate", ex);
                 }
             }
             catch(Exception ex)
             {
-
+                Library.WriteLog("At updating user details",ex);
             }
 
             return StatusCode(HttpStatusCode.NoContent);
@@ -252,11 +219,19 @@ namespace Classigoo.Controllers
         [ActionName("GetMyAdds")]
         public IHttpActionResult GetMyAdds(Guid userId)
         {
-            List<Add> adds = db.Adds.Where(a => a.UserId == userId).OrderByDescending(add=>add.Created).ToList();
             List<CustomAdd> addColl = new List<CustomAdd>();
-            foreach(Add add in adds)
+            try
             {
-                addColl.Add(CheckCategory(add));
+                List<Add> adds = db.Adds.Where(a => a.UserId == userId).OrderByDescending(add => add.Created).ToList();
+               
+                foreach (Add add in adds)
+                {
+                    addColl.Add(CheckCategory(add));
+                }
+            }
+            catch(Exception ex)
+            {
+                Library.WriteLog("At GetMyadds for userid- " + userId, ex);
             }
             if(addColl.Count>0)
             return Ok(addColl);
@@ -267,7 +242,15 @@ namespace Classigoo.Controllers
         [ActionName("GetAddById")]
         public IHttpActionResult GetAddById(int addId)
         {
-            Add add = db.Adds.Find(addId);
+            Add add = new Add();
+            try
+            {
+                 add = db.Adds.Find(addId);
+            }
+            catch(Exception ex)
+            {
+                Library.WriteLog("At Getting add by id- "+addId, ex);
+            }
             if (add!=null)
                 return Ok(add);
             else
@@ -276,68 +259,75 @@ namespace Classigoo.Controllers
         public CustomAdd CheckCategory(Add add)
         {
             CustomAdd customAdd = new CustomAdd();
-            customAdd.Location = add.Mandal + "," + add.State;
-
-            DateTime dtTemp = add.Created.Value;
-
-            customAdd.CreatedDate = dtTemp.ToString("MMMM") + ", " + dtTemp.Day + ", " + dtTemp.Year; // .mon.ToLongDateString();
-            customAdd.AddId = add.AddId;
-            customAdd.Title = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(add.Title);
-            customAdd.Category = add.Category;
-            customAdd.Status = add.Status;
-            switch (add.Category)
+            try
             {
-                case Constants.RealEstate:
-                    {
-                        foreach (var item in add.RealEstates)
-                        {
-                            customAdd.Description = item.Description;
-                            customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
-                            customAdd.Price = item.Price;
-                        }
-                        break;
-                    }
-                case Constants.TransportationVehicle:
-                    {
-                        foreach (var item in add.TransportationVehicles)
-                        {
-                            customAdd.Description = item.Description;
-                            customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
-                            customAdd.Price = item.Price;
-                        }
-                        break;
-                    }
-                case Constants.ConstructionVehicle:
-                    {
-                        foreach (var item in add.ConstructionVehicles)
-                        {
-                            customAdd.Description = item.Description;
-                            customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
-                            customAdd.Price = item.Price;
-                        }
-                        break;
-                    }
-                case Constants.AgriculturalVehicle:
-                    {
-                        foreach (var item in add.AgriculturalVehicles)
-                        {
-                            customAdd.Description = item.Description;
-                            customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
-                            customAdd.Price = item.Price;
-                        }
-                        break;
-                    }
-                case Constants.PassengerVehicle:
-                    {
-                        foreach (var item in add.PassengerVehicles)
-                        {
-                            customAdd.Description = item.Description;
-                            customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
-                            customAdd.Price = item.Price;
-                        }
-                        break;
-                    }
+                customAdd.Location = add.Mandal + "," + add.State;
 
+                DateTime dtTemp = add.Created.Value;
+
+                customAdd.CreatedDate = dtTemp.ToString("MMMM") + ", " + dtTemp.Day + ", " + dtTemp.Year; // .mon.ToLongDateString();
+                customAdd.AddId = add.AddId;
+                customAdd.Title = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(add.Title);
+                customAdd.Category = add.Category;
+                customAdd.Status = add.Status;
+                switch (add.Category)
+                {
+                    case Constants.RealEstate:
+                        {
+                            foreach (var item in add.RealEstates)
+                            {
+                                customAdd.Description = item.Description;
+                                customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
+                                customAdd.Price = item.Price;
+                            }
+                            break;
+                        }
+                    case Constants.TransportationVehicle:
+                        {
+                            foreach (var item in add.TransportationVehicles)
+                            {
+                                customAdd.Description = item.Description;
+                                customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
+                                customAdd.Price = item.Price;
+                            }
+                            break;
+                        }
+                    case Constants.ConstructionVehicle:
+                        {
+                            foreach (var item in add.ConstructionVehicles)
+                            {
+                                customAdd.Description = item.Description;
+                                customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
+                                customAdd.Price = item.Price;
+                            }
+                            break;
+                        }
+                    case Constants.AgriculturalVehicle:
+                        {
+                            foreach (var item in add.AgriculturalVehicles)
+                            {
+                                customAdd.Description = item.Description;
+                                customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
+                                customAdd.Price = item.Price;
+                            }
+                            break;
+                        }
+                    case Constants.PassengerVehicle:
+                        {
+                            foreach (var item in add.PassengerVehicles)
+                            {
+                                customAdd.Description = item.Description;
+                                customAdd.ImgUrlPrimary = item.ImgUrlPrimary;
+                                customAdd.Price = item.Price;
+                            }
+                            break;
+                        }
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Library.WriteLog("Checking Category for getmyadds", ex);
             }
             return customAdd;
         }
@@ -345,33 +335,36 @@ namespace Classigoo.Controllers
         [ActionName("Admin")]
         public  IHttpActionResult Admin()
         {
-            var addColl = (from add in db.Adds 
-                            select new
-                            {
-                                AddId = add.AddId,
-                                Created = add.Created,
-                                Category = add.Category,
-                                State = add.State,
-                                District = add.District,
-                                Mandal = add.Mandal,
-                                Status = add.Status
-                            }).OrderByDescending(add=>add.Created).ToList()
-                            .Select(add=> new Add()
-                            {
-                           AddId = add.AddId,
-                           Created = add.Created,
-                           Category = add.Category,
-                           State = add.State,
-                           District = add.District,
-                           Mandal = add.Mandal,
-                          Status = add.Status
-                          });
-            if (addColl.Count() > 0)
-            {
-                return Ok(addColl);
-            }
-            else
-                return NotFound();
+            
+              var   addColl = (from add in db.Adds
+                               select new
+                               {
+                                   AddId = add.AddId,
+                                   Created = add.Created,
+                                   Category = add.Category,
+                                   State = add.State,
+                                   District = add.District,
+                                   Mandal = add.Mandal,
+                                   Status = add.Status
+                               }).OrderByDescending(add => add.Created).ToList()
+                                .Select(add => new Add()
+                                {
+                                    AddId = add.AddId,
+                                    Created = add.Created,
+                                    Category = add.Category,
+                                    State = add.State,
+                                    District = add.District,
+                                    Mandal = add.Mandal,
+                                    Status = add.Status
+                                });
+            
+                if (addColl.Count() > 0)
+                {
+                    return Ok(addColl);
+                }
+                else
+                    return NotFound();
+         
         }
         [HttpPut]
         [ActionName("UpdateAddStatus")]
@@ -381,24 +374,24 @@ namespace Classigoo.Controllers
             {
                 Add add = db.Adds.Find(addId);
                 add.Status = status;
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
+                //if (!ModelState.IsValid)
+                //{
+                //    return BadRequest(ModelState);
+                //}
                 db.Entry(add).State = EntityState.Modified;
 
                 try
                 {
                     db.SaveChanges();
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateConcurrencyException ex)
                 {
-                    
+                    Library.WriteLog("At Updating Add Status- db update", ex);
                 }
             }
             catch (Exception ex)
             {
-
+                Library.WriteLog("At Updating Add Status", ex);
             }
 
             return StatusCode(HttpStatusCode.NoContent);
